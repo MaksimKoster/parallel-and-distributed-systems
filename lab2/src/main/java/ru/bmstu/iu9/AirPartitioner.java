3c3112f462d435c8c3761a0eb116d2c7bd55d35f
MaksimKoster <m.kostritsya@yandex.ru>
2021-12-14 02:02:42 +0300

    Scripted auto-commit on change (2021-12-14 02:02:42) by gitwatch.sh

--- lab2/src/main/java/ru/bmstu/iu9/AirPartitioner.java
+++ lab2/src/main/java/ru/bmstu/iu9/AirPartitioner.java
@@ -4,5 +4,6 @@ import org.apache.hadoop.io.Text;
 import org.apache.mapreduce.Partitioner;
 
 public class AirPartitioner extends Partitioner<AirWritableComparable, Text>{
-    
+    @Override
+    public int getPartition()
 }
