371f9ab2318a23570fe73d9f8ea74d22d23b8462
MaksimKoster <m.kostritsya@yandex.ru>
2021-12-14 01:23:10 +0300

    Scripted auto-commit on change (2021-12-14 01:23:10) by gitwatch.sh

--- lab2/src/main/java/ru/bmstu/iu9/AirWritableComparable.java
+++ lab2/src/main/java/ru/bmstu/iu9/AirWritableComparable.java
@@ -1,7 +1,34 @@
 package ru.bmstu.iu9;
 
+import java.io.*;
+import java.util.Objects;
+
 import org.apache.hadoop.io.WritableComparable;
 
 public class AirWritableComparable implements WritableComparable{
-    private int airpo
+    private int airportID;
+    private int indicator;
+
+    public void setAirportID(int airportID){
+        this.airportID = airportID;
+    }
+
+    public void setIndicator(int indicator){
+        this.indicator = indicator;
+    }
+
+    public int getAirportID(){
+        return airportID;
+    }
+
+    public int getIndicator(){
+        return indicator;
+    }
+
+    public AirWritableComparable(int airportID, int indicator){
+        this.airportID = airportID;
+        this.indicator = indicator;
+    }
+
+    @override 
 }
