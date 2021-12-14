3def3931e49358ea81ea94996451d8ee68925bc7
MaksimKoster <m.kostritsya@yandex.ru>
2021-12-14 14:02:54 +0300

    Scripted auto-commit on change (2021-12-14 14:02:54) by gitwatch.sh

--- lab2/src/main/java/ru/bmstu/iu9/AirMapper.java
+++ lab2/src/main/java/ru/bmstu/iu9/AirMapper.java
@@ -11,10 +11,13 @@ public class AirMapper extends Mapper<LongWritable, Text, AirWritableComparable,
     public static int AIRPORT_INDICATOR = 0;
     public static int ID_INDEX = 0;
     public static int NAME_INDEX = 1;
-    public static String SPLIT = "\",\"";
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
-        String[] params = value.toString().split(SPLIT);
+        String[] params = value.toString().split("\",\"");
+        int airID;
+        if (key.get() > 0){
+            airID = Integer.parseInt(params[ID_INDEX].split("\""))
+        }
     }
 }
