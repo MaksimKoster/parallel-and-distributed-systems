7555f1c4fb12494f755aec8b4b2257c1cb236951
MaksimKoster <m.kostritsya@yandex.ru>
2021-12-14 15:08:29 +0300

    Scripted auto-commit on change (2021-12-14 15:08:29) by gitwatch.sh

--- lab2/src/main/java/ru/bmstu/iu9/AirApp.java
+++ lab2/src/main/java/ru/bmstu/iu9/AirApp.java
@@ -1,4 +1,22 @@
 package ru.bmstu.iu9;
 
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.Job;
+
 public class AirApp {
+    public static void main(String[] args) throws Exception{
+        if (args.length != 3) {
+            System.err.println("Usage: AirApp <flight data path> <airports data path> <output path>");
+            System.exit(-1);
+        }
+        Job job = Job.getInstance();
+        job.setJarByClass(AirApp.class);
+        job.setJobName("Join App");
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, FlightMapper.class);
+        
+    }
 }
