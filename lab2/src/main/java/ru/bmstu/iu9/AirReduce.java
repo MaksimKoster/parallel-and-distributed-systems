651c9f9255d6c567817d5e8d6d701e7f972a2a38
MaksimKoster <m.kostritsya@yandex.ru>
2021-12-14 14:35:37 +0300

    Scripted auto-commit on change (2021-12-14 14:35:37) by gitwatch.sh

--- lab2/src/main/java/ru/bmstu/iu9/AirReduce.java
+++ lab2/src/main/java/ru/bmstu/iu9/AirReduce.java
@@ -11,12 +11,25 @@ public class AirReduce extends Reducer<AirWritableComparable, Text, Text, Text> 
     protected void reduce(AirWritableComparable key, Iterable<Text> values,Context context) throws IOException, InterruptedException {
         Iterator<Text> valuesIter = values.iterator();
         String air = valuesIter.next().toString();
-        float max, sum, count = 0.0f;
+        float max = 0.0f;
+        float sum = 0.0f;
+        int count = 0;
         float min = -1.0f;
 
         while(valuesIter.hasNext()){
             float delayTime = Float.parseFloat(valuesIter.next().toString());
-            
+            if (delayTime > max){
+                max = delayTime;
+            }
+            if(min == -1 || delayTime < min){
+                min = delayTime;
+            }
+            sum+= delayTime;
+            count++;
+        }
+        if (count > 0 ){
+            context.write(new Text("Airport name:" + air) ,
+                            new Text("min: " + min + ", max: " + max + ",average: " + sum/count));
         }
 
     }
