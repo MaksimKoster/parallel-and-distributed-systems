a8dd6e2be91f1eb2c3b1ac92e35267e49d0feb7e
MaksimKoster <m.kostritsya@yandex.ru>
2021-12-14 14:28:17 +0300

    Scripted auto-commit on change (2021-12-14 14:28:17) by gitwatch.sh

--- lab2/src/main/java/ru/bmstu/iu9/AirReduce.java
+++ lab2/src/main/java/ru/bmstu/iu9/AirReduce.java
@@ -1,4 +1,22 @@
 package ru.bmstu.iu9;
 
-public class AirReduce {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+public class AirReduce extends Reducer<AirWritableComparable, Text, Text, Text> {
+    @Override
+    protected void reduce(AirWritableComparable key, Iterable<Text> values,Context context) throws IOException, InterruptedException {
+        Iterator<Text> valuesIter = values.iterator();
+        String air = valuesIter.next().toString();
+        float max, sum, count = 0.0f;
+        float min = -1.0f;
+
+        while(valuesIter.hasNext()){
+            float delayTime = Float.parseFloat()
+        }
+
+    }
 }
