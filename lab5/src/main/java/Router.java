5fba9b3915dc67cfa99ff7b439a48d6483cf4fbb
MaksimKoster <m.kostritsya@yandex.ru>
2021-12-17 02:52:39 +0300

    Scripted auto-commit on change (2021-12-17 02:52:39) by gitwatch.sh

--- lab5/src/main/java/Router.java
+++ lab5/src/main/java/Router.java
@@ -1,2 +1,25 @@
+import akka.NotUsed;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.server.Route;
+
+import akka.stream.ActorMaterializer;
+
+import java.time.Duration;
+
+import static akka.http.javadsl.server.Directives.*;
+
 public class Router {
+
+    static Duration TIMEOUT = Duration.ofSeconds(5);
+    public static String PARAMETR_URL = "testUrl";
+    public static String PARAMETR_COUNT = "count";
+
+    public static Route createRoute(ActorMaterializer materializer, ActorSystem system, Http http, ActorRef cacheActor){
+        return route(get(()->
+                parameter(PARAMETR_URL)
+                )
+    }
+
 }
