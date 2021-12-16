5e5d9ea156145b06a6e8b065bef830454aa5f411
MaksimKoster <m.kostritsya@yandex.ru>
2021-12-16 15:49:23 +0300

    Scripted auto-commit on change (2021-12-16 15:49:22) by gitwatch.sh

--- lab3/src/main/java/App.java
+++ lab3/src/main/java/App.java
@@ -1,6 +1,13 @@
+import org.apache.spark.SparkConf;
+
 public class App {
-    static String APP_NAME;
-    static String INPUT_AIRPORT;
-    static String INPUT_FLIGHT;
-    static String 
+    static String APP_NAME = "lab3";
+    static String INPUT_AIRPORT = "L_AIRPORT_ID.csv";
+    static String INPUT_FLIGHT = "664600583_T_ONTIME_sample.csv";
+    static String OUTPUT = "output";
+
+    public static void main(String[] args){
+        SparkConf conf = SparkCond().setAppname(APP_NAME);
+    }
+
 }
