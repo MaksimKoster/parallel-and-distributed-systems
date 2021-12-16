b98868b758052d719c6202531d3f7ad6bfded088
MaksimKoster <m.kostritsya@yandex.ru>
2021-12-16 17:00:50 +0300

    Scripted auto-commit on change (2021-12-16 17:00:50) by gitwatch.sh

--- lab3/src/main/java/FlightReduce.java
+++ lab3/src/main/java/FlightReduce.java
@@ -1,2 +1,8 @@
-public class FlightReduce {
+import scala.Serializable;
+
+public class FlightReduce implements Serializable {
+    public float maxDelay;
+    public int countDelays;
+    public int countCancelled;
+    public int countFlights;
 }
