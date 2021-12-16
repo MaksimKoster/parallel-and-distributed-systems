206d6a2a8cd80f5637397969b226874fe0d5ba21
MaksimKoster <m.kostritsya@yandex.ru>
2021-12-17 02:27:58 +0300

    Scripted auto-commit on change (2021-12-17 02:27:58) by gitwatch.sh

--- lab5/src/main/java/CachingActor.java
+++ lab5/src/main/java/CachingActor.java
@@ -2,5 +2,36 @@ import akka.actor.AbstractActor;
 
 public class CachingActor extends AbstractActor {
 
-    
+    @Override
+    public Receive createRecieve()
+
+    public static class GetMessage{
+        private String url;
+
+        public String getUrl(){
+            return url;
+        }
+
+        GetMessage(String url){
+            this.url = url;
+        }
+    }
+
+    public static class StoreMessage{
+        private String url;
+        private Long res;
+
+        public String getUrl(){
+            return url;
+        }
+
+        public Long getResult(){
+            return res;
+        }
+
+        StoreMessage(String url, Long res){
+            this.url = url;
+            this.res = res;
+        }
+    }
 }
