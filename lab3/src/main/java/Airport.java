4e11db8df44c1c7fc64737250e9d07ca23e530e0
MaksimKoster <m.kostritsya@yandex.ru>
2021-12-16 16:28:42 +0300

    Scripted auto-commit on change (2021-12-16 16:28:42) by gitwatch.sh

--- lab3/src/main/java/Airport.java
+++ lab3/src/main/java/Airport.java
@@ -1,2 +1,27 @@
-public class Airport {
+import scala.Tuple2;
+import scala.Serializable;
+
+public class Airport implements Serializable {
+    public int code;
+    public String name;
+
+    public static int CODE_INDEX = 0;
+    public static int NAME_INDEX = 1;
+
+    public Airport(int code, String name){
+        this.code = code;
+        this.name = name;
+    }
+
+    public static Airport parseCSV(String csv){
+        String[] list = csv.split(",", 2);
+        return new Airport(
+                Integer.parseInt(list[CODE_INDEX].replaceAll("\"","")),
+                list[NAME_INDEX]
+        );
+    }
+
+    public final Tuple2<Integer, String> getTuple(){
+        return new Tuple2<>(code, name);
+    }
 }
