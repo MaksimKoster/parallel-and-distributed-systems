53884ce8458c76337082b9145878ed0a1b74c4ed
MaksimKoster <m.kostritsya@yandex.ru>
2021-12-18 02:53:52 +0300

    Scripted auto-commit on change (2021-12-18 02:53:52) by gitwatch.sh

--- lab4/src/main/java/StoreActor.java
+++ lab4/src/main/java/StoreActor.java
@@ -2,6 +2,13 @@ import akka.actor.AbstractActor;
 
 public class StoreActor extends AbstractActor {
 
+    public static class StoreMessage{
+        private int packetID;
+        private String res;
+
+
+    }
+
     @Override
     public Receive createReceive() {
         return null;
