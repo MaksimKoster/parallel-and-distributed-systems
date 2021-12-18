d29eb24e948f953b0dab9069d41123d8bb720309
MaksimKoster <m.kostritsya@yandex.ru>
2021-12-18 03:08:26 +0300

    Scripted auto-commit on change (2021-12-18 03:08:26) by gitwatch.sh

--- lab4/src/main/java/StoreActor.java
+++ lab4/src/main/java/StoreActor.java
@@ -1,6 +1,14 @@
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class StoreActor extends AbstractActor {
+    public static final String RECIEVE_MESSAGE = "recieve test";
+
+    private Map<Integer, ArrayList<String>> store = new HashMap<>();
 
     public static class StoreMessage{
         private int packetID;
@@ -18,11 +26,33 @@ public class StoreActor extends AbstractActor {
         public int getPacketID(){
             return packetID;
         }
+    }
 
+    public static class GetMessage{
+        private int packetID;
+
+        GetMessage(int packetID){
+            this.packetID = packetID;
+        }
+
+        public int getPacketID(){
+            return packetID;
+        }
     }
 
     @Override
     public Receive createReceive() {
-        return null;
+        return ReceiveBuilder.create()
+                .match(StoreMessage.class, m -> {
+                    ArrayList<String> res = store.get(m.getPacketID());
+                    if (res == null){
+                        res = new ArrayList<>(0);
+                    }
+                    res.add(m.getRes());
+                    System.out.println(m.getRes());
+                    store.put(m.getPacketID(), res);
+                    System.out.println(RECIEVE_MESSAGE);
+                })
+                .build();
     }
 }
