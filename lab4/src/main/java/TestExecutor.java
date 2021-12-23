213b0c505efc8f54992e92db2d758821eb0c3fa3
MaksimKoster <m.kostritsya@yandex.ru>
2021-12-24 01:23:07 +0300

    Scripted auto-commit on change (2021-12-24 01:23:07) by gitwatch.sh

--- lab4/src/main/java/TestExecutor.java
+++ lab4/src/main/java/TestExecutor.java
@@ -1,9 +1,24 @@
 import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+
+import java.util.ArrayList;
 
 public class TestExecutor extends AbstractActor {
+    ActorRef storeActore;
+
+    TestExecutor(ActorRef storeActore){
+        this.storeActore = storeActore;
+    }
 
     @Override
     public Receive createReceive() {
         return null;
+    }
+
+    public static class Message{
+        private int packageid;
+        private String jsScript;
+        private String functionName;
+        private ArrayList<PackageTests.Test> tests;
     }
 }
