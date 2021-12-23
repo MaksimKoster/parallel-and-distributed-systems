f18fdc014cb3175fb364cd1e9fd3eb4dae2bac40
MaksimKoster <m.kostritsya@yandex.ru>
2021-12-24 00:38:52 +0300

    Scripted auto-commit on change (2021-12-24 00:38:52) by gitwatch.sh

--- lab4/src/main/java/PackageTests.java
+++ lab4/src/main/java/PackageTests.java
@@ -1,7 +1,8 @@
 public class PackageTests {
     public static final String FUNCTION_NAME = "functionName";
-    public static final String FUNCTION_NAME = "functionName";
-    public static final String FUNCTION_NAME = "functionName";
-    public static final String FUNCTION_NAME = "functionName";
-    public static final String FUNCTION_NAME = "functionName";
+    public static final String JS_SCRIPT = "jsScript";
+    public static final String TESTS = "tests";
+    public static final String EXPECTED_RESULT = "expectedResult";
+    public static final String PARAMS = "params";
+    public static final String TEST_NAME = "testName";
 }
