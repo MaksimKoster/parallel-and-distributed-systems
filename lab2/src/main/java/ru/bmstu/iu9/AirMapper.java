e87809a2c2c67b051459a2e2323537e05ecf9f46
MaksimKoster <m.kostritsya@yandex.ru>
2021-12-25 13:24:50 +0300

    Scripted auto-commit on change (2021-12-25 13:24:50) by gitwatch.sh

--- lab2/src/main/java/ru/bmstu/iu9/AirMapper.java
+++ lab2/src/main/java/ru/bmstu/iu9/AirMapper.java
@@ -12,7 +12,7 @@ public class AirMapper extends Mapper<LongWritable, Text, AirWritableComparable,
     public static int AIRPORT_INDICATOR = 0;
     public static int ID_INDEX = 0;
     public static int NAME_INDEX = 1;
-    public static String REGEX = "\"\",\"\"";
+    public static String REGEX = ",";
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
@@ -21,7 +21,7 @@ public class AirMapper extends Mapper<LongWritable, Text, AirWritableComparable,
         if (key.get() > 0){
             String airName;
             int airID;
-            airID = Integer.parseInt(params[ID_INDEX].split("\"")[1]);
+            airID = Integer.parseInt(params[ID_INDEX]);
             airName = params[NAME_INDEX].split("\"")[0];
             context.write(new AirWritableComparable(airID, AIRPORT_INDICATOR), new Text(airName));
         }
