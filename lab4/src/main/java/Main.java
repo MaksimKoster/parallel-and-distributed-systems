d7610026fc4c61b88e389294884f1c05adee4a1b
MaksimKoster <m.kostritsya@yandex.ru>
2021-12-25 09:22:53 +0300

    Scripted auto-commit on change (2021-12-25 09:22:53) by gitwatch.sh

--- lab4/src/main/java/Main.java
+++ lab4/src/main/java/Main.java
@@ -1,6 +1,7 @@
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.actor.SupervisorStrategy;
 
 import java.io.IOException;
 
@@ -9,6 +10,6 @@ public class Main {
     public static void main(String[] args) throws IOException{
         ActorSystem system = ActorSystem.create("routes");
         ActorRef storeActor = system.actorOf(Props.create(StoreActor.class));
-        
+        final SupervisorStrategy strategy
     }
 }
