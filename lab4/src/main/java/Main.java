97151c28e951225f75e1dd247d293f51222e1fed
MaksimKoster <m.kostritsya@yandex.ru>
2021-12-25 09:45:20 +0300

    Scripted auto-commit on change (2021-12-25 09:45:20) by gitwatch.sh

--- lab4/src/main/java/Main.java
+++ lab4/src/main/java/Main.java
@@ -18,7 +18,7 @@ public class Main {
                 5,
                 Duration.ofMinutes(1),
                 Collections.<Class <? extends  Throwable>> singletonList(Exception.class));
-        ActorRef router = system.actorOf(new RoundRobinPool(5).withSupervisorStrategy(strategy).props());
+        ActorRef router = system.actorOf(new RoundRobinPool(5).withSupervisorStrategy(strategy).props(Props.create(TestExecutor.class, storeActor)));
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         MainHttp instance = new MainHttp(system, storeActor, router);
