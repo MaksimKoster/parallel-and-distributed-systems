4f79b284bbb4ec5ccfdf668d1fd8f170ec9a36f9
MaksimKoster <m.kostritsya@yandex.ru>
2021-12-25 03:36:55 +0300

    Scripted auto-commit on change (2021-12-25 03:36:55) by gitwatch.sh

--- lab4/src/main/java/MainHttp.java
+++ lab4/src/main/java/MainHttp.java
@@ -1,7 +1,13 @@
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+
 import java.time.Duration;
 
 public class MainHttp {
     static final Duration TIMEOUT = Duration.ofSeconds(5);
+    public static final String TEST_EXEC = "test execution started";
 
-    
+    ActorSystem system;
+    ActorRef storeActor;
+
 }
