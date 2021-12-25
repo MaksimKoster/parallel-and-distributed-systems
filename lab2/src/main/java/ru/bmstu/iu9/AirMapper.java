922c94b5fe80daf2ab25f48645400525db376fc7
MaksimKoster <m.kostritsya@yandex.ru>
2021-12-25 12:53:11 +0300

    Scripted auto-commit on change (2021-12-25 12:53:10) by gitwatch.sh

--- lab2/src/main/java/ru/bmstu/iu9/AirMapper.java
+++ lab2/src/main/java/ru/bmstu/iu9/AirMapper.java
@@ -17,18 +17,10 @@ public class AirMapper extends Mapper<LongWritable, Text, AirWritableComparable,
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
         String[] params = value.toString().split(REGEX);
-        FileWriter writer = new FileWriter("test.txt");
 
         if (key.get() > 0){
             String airName;
             int airID;
-
-            writer.write(params[ID_INDEX]);
-            writer.write(params[ID_INDEX].split("\"")[1]);
-            writer.write(params[NAME_INDEX]);
-            writer.write(params[NAME_INDEX].split("\"")[0]);
-
-
             airID = Integer.parseInt(params[ID_INDEX].split("\"")[1]);
             airName = params[NAME_INDEX].split("\"")[0];
             context.write(new AirWritableComparable(airID, AIRPORT_INDICATOR), new Text(airName));
