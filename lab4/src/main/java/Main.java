47e831a8eec36a65470ee1a68e62d745eb94c7a2
MaksimKoster <m.kostritsya@yandex.ru>
2021-12-25 09:20:00 +0300

    Scripted auto-commit on change (2021-12-25 09:20:00) by gitwatch.sh

--- lab4/src/main/java/Main.java
+++ lab4/src/main/java/Main.java
@@ -1,8 +1,10 @@
+import akka.actor.ActorSystem;
+
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException{
-        
+        ActorSystem system = ActorSystem.create("routes");
     }
 }
