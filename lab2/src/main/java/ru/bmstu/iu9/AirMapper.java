90d05cfe3eda091445ecd127f05358bc01a61185
MaksimKoster <m.kostritsya@yandex.ru>
2022-01-18 05:03:16 +0300

    Scripted auto-commit on change (2022-01-18 05:03:16) by gitwatch.sh

--- lab2/src/main/java/ru/bmstu/iu9/AirMapper.java
+++ lab2/src/main/java/ru/bmstu/iu9/AirMapper.java
@@ -4,7 +4,6 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-import java.io.FileWriter;
 import java.io.IOException;
 
 
@@ -15,12 +14,12 @@ public class AirMapper extends Mapper<LongWritable, Text, AirWritableComparable,
     public static String SPLIT = "\",";
 
 
+
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
         String[] params = value.toString().split(SPLIT);
 
         if (key.get() > 0){
-            String airName;
             int airID = Integer.parseInt(params[ID_INDEX].replaceAll("\"",""));
             airName = params[NAME_INDEX];
             context.write(new AirWritableComparable(airID, AIRPORT_INDICATOR), new Text(airName));
