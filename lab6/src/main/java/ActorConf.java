42198e10f583187edd67851d660f1cb554929bba
MaksimKoster <m.kostritsya@yandex.ru>
2022-01-19 17:20:49 +0300

    Scripted auto-commit on change (2022-01-19 17:20:49) by gitwatch.sh

--- lab6/src/main/java/ActorConf.java
+++ lab6/src/main/java/ActorConf.java
@@ -4,5 +4,9 @@ import java.util.ArrayList;
 
 public class ActorConf extends AbstractActor {
     private ArrayList<String> servers = new ArrayList<>();
-    
+
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
