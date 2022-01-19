ee9aa2c552480173c0260ff10505d9b7351b9e94
MaksimKoster <m.kostritsya@yandex.ru>
2022-01-19 18:06:27 +0300

    Scripted auto-commit on change (2022-01-19 18:06:27) by gitwatch.sh

--- lab6/src/main/java/HttpServer.java
+++ lab6/src/main/java/HttpServer.java
@@ -2,16 +2,16 @@ import akka.actor.ActorRef;
 import akka.http.javadsl.Http;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
 
 public class HttpServer implements Watcher {
 
     private final Http http;
     private final ActorRef actConf;
     private final ZooKeeper zoo;
-    private String Path
+    private String Path;
 
-
-    HttpServer()
+    HttpServer(Http http, ActorRef actConf, ZooKeeper zoo, String port)
 
     @Override
     public void process(WatchedEvent watchedEvent) {
