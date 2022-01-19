462598676c7b892079ea0c33d3c168307fd744ba
MaksimKoster <m.kostritsya@yandex.ru>
2022-01-19 18:53:53 +0300

    Scripted auto-commit on change (2022-01-19 18:53:53) by gitwatch.sh

--- lab6/src/main/java/ActorSys/ActorConf.java
+++ lab6/src/main/java/ActorSys/ActorConf.java
@@ -3,6 +3,7 @@ package ActorSys;
 import akka.actor.AbstractActor;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 final class MessageGetRandomServerUrl{
     MessageGetRandomServerUrl(){}
@@ -10,13 +11,20 @@ final class MessageGetRandomServerUrl{
 
 public class ActorConf extends AbstractActor {
     private ArrayList<String> servers = new ArrayList<>();
+    private Random rand = Random();
 
     @Override
     public Receive createReceive() {
         return receiveBuilder()
                 .match(
                         MessageGetRandomServerUrl.class,
-                        
+                        msg -> sender().tell(
+                                g
+                        )
                 )
+    }
+
+    public getRandomPort(){
+
     }
 }
