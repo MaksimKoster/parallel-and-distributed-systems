3afab9b5d4b7511655105ff263e11a03b12f58f4
MaksimKoster <m.kostritsya@yandex.ru>
2022-01-19 18:48:57 +0300

    Scripted auto-commit on change (2022-01-19 18:48:57) by gitwatch.sh

--- lab6/src/main/java/ActorSys/ActorConf.java
+++ lab6/src/main/java/ActorSys/ActorConf.java
@@ -2,7 +2,10 @@ package ActorSys;
 
 import akka.actor.AbstractActor;
 
+import java.util.ArrayList;
+
 public class ActorConf extends AbstractActor {
+    private ArrayList<String> servers = new ArrayList<>();
 
     @Override
     public Receive createReceive() {
