f65ff34015d7c97ba7cf8605780fc5034464e0ec
MaksimKoster <m.kostritsya@yandex.ru>
2022-01-19 18:23:57 +0300

    Scripted auto-commit on change (2022-01-19 18:23:57) by gitwatch.sh

--- lab6/src/main/java/AnonymizeApp.java
+++ lab6/src/main/java/AnonymizeApp.java
@@ -1,3 +1,5 @@
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
 import org.apache.log4j.BasicConfigurator;
 
 public class AnonymizeApp {
@@ -9,6 +11,6 @@ public class AnonymizeApp {
         }
         BasicConfigurator.configure();
         System.out.println("start anonymize app");
-        
+        ActorSystem sys = ActorSystem.create("lab6")
     }
 }
