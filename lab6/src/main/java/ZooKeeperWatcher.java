c92194d60fae10ec45f4d6a7dfd229962edf76af
MaksimKoster <m.kostritsya@yandex.ru>
2022-01-19 16:44:28 +0300

    Scripted auto-commit on change (2022-01-19 16:44:28) by gitwatch.sh

--- lab6/src/main/java/ZooKeeperWatcher.java
+++ lab6/src/main/java/ZooKeeperWatcher.java
@@ -1,5 +1,12 @@
+import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 
 public class ZooKeeperWatcher implements Watcher {
-    public static String SERVERS_PATH = "/servers"
+    public static String SERVERS_PATH = "/servers";
+
+
+    @Override
+    public void process(WatchedEvent watchedEvent) {
+
+    }
 }
