c74bd696bd3eee1f3fcd7474fb3a4d90adf4c26f
MaksimKoster <m.kostritsya@yandex.ru>
2022-01-19 17:12:46 +0300

    Scripted auto-commit on change (2022-01-19 17:12:46) by gitwatch.sh

--- lab6/src/main/java/MessageSendServersList.java
+++ lab6/src/main/java/MessageSendServersList.java
@@ -1,5 +1,9 @@
 import java.util.ArrayList;
 
 public class MessageSendServersList {
-    private final ArrayList<String> servers
+    private final ArrayList<String> servers;
+
+    MessageSendServersList(ArrayList<String> serv){
+        this.servers = serv;
+    }
 }
