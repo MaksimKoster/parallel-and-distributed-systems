04ba6a10feaec8e987d224f0f7d9f5b33a91eeb1
MaksimKoster <m.kostritsya@yandex.ru>
2022-01-19 18:04:06 +0300

    Scripted auto-commit on change (2022-01-19 18:04:06) by gitwatch.sh

--- lab6/src/main/java/HttpServer.java
+++ lab6/src/main/java/HttpServer.java
@@ -1,7 +1,13 @@
+import akka.http.javadsl.Http;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 
 public class HttpServer implements Watcher {
+
+    private final Http http;
+
+
+    HttpServer()
 
     @Override
     public void process(WatchedEvent watchedEvent) {
