3253b3d26e57911579fa6eb92be6199ac591250f
MaksimKoster <m.kostritsya@yandex.ru>
2022-01-19 18:28:55 +0300

    Scripted auto-commit on change (2022-01-19 18:28:55) by gitwatch.sh

--- lab6/src/main/java/AnonymizeApp.java
+++ lab6/src/main/java/AnonymizeApp.java
@@ -1,6 +1,7 @@
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.stream.ActorMaterializer;
 import org.apache.log4j.BasicConfigurator;
 
 public class AnonymizeApp {
@@ -14,6 +15,6 @@ public class AnonymizeApp {
         System.out.println("start anonymize app");
         ActorSystem sys = ActorSystem.create("lab6");
         ActorRef actConf = sys.actorOf(Props.create(ActorSysMes.ActorConf.class));
-
+        final ActorMaterializer materializer
     }
 }
