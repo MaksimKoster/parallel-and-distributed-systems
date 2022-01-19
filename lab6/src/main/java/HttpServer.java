9de40ad2c9d36630431d3baf51d3eb9ae4c9d6cf
MaksimKoster <m.kostritsya@yandex.ru>
2022-01-19 19:09:59 +0300

    Scripted auto-commit on change (2022-01-19 19:09:59) by gitwatch.sh

--- lab6/src/main/java/HttpServer.java
+++ lab6/src/main/java/HttpServer.java
@@ -1,4 +1,9 @@
+import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 
 public class HttpServer implements Watcher {
+    @Override
+    public void process(WatchedEvent watchedEvent) {
+
+    }
 }
