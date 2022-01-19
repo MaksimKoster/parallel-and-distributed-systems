5183b3f58da5d86e37ccb65205258c9df700e9f1
MaksimKoster <m.kostritsya@yandex.ru>
2022-01-19 19:19:20 +0300

    Scripted auto-commit on change (2022-01-19 19:19:20) by gitwatch.sh

--- lab6/src/main/java/AnonymizeApp.java
+++ lab6/src/main/java/AnonymizeApp.java
@@ -1,5 +1,7 @@
 public class AnonymizeApp {
     public static void main(String[] args){
-        
+        if (args.length < 2){
+            System.err.println("No port argument");
+        }
     }
 }
