a4f1f5e87ba10979479a1539ed92dae701cc58f1
MaksimKoster <m.kostritsya@yandex.ru>
2022-01-19 16:46:04 +0300

    Scripted auto-commit on change (2022-01-19 16:46:04) by gitwatch.sh

--- lab6/src/main/java/ZooKeeperWatcher.java
+++ lab6/src/main/java/ZooKeeperWatcher.java
@@ -1,9 +1,12 @@
+import akka.actor.ActorRef;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
 
 public class ZooKeeperWatcher implements Watcher {
     public static String SERVERS_PATH = "/servers";
 
+    ZooKeeperWatcher(ZooKeeper zoo, ActorRef actConfig)
 
     @Override
     public void process(WatchedEvent watchedEvent) {
