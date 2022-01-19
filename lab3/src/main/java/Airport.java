4742daaa30de74bd1c73360cbd96644b73c31c6a
MaksimKoster <m.kostritsya@yandex.ru>
2022-01-19 15:43:20 +0300

    Scripted auto-commit on change (2022-01-19 15:43:20) by gitwatch.sh

--- lab3/src/main/java/Airport.java
+++ lab3/src/main/java/Airport.java
@@ -14,7 +14,7 @@ public class Airport implements Serializable {
     }
 
     protected Pair<Integer,String> SplitDataToCodeName(String data, String reg){
-        Pair ans
+        Pair<Integer, String> ans;
         String[] params = data.split(",", 2);
         params[CODE_INDEX].replaceAll("\"","");
     }
