4d4b169de99adf2a9215609e03de67578e06b9d7
MaksimKoster <m.kostritsya@yandex.ru>
2022-01-19 17:20:26 +0300

    Scripted auto-commit on change (2022-01-19 17:20:26) by gitwatch.sh

--- lab6/src/main/java/ActorConf.java
+++ lab6/src/main/java/ActorConf.java
@@ -3,5 +3,6 @@ import akka.actor.AbstractActor;
 import java.util.ArrayList;
 
 public class ActorConf extends AbstractActor {
-    private ArrayList<String> servers
+    private ArrayList<String> servers = new ArrayList<>();
+    
 }
