da715db3a34a6d7c33c33643cbf4121383a50ae2
MaksimKoster <m.kostritsya@yandex.ru>
2022-01-19 18:52:18 +0300

    Scripted auto-commit on change (2022-01-19 18:52:18) by gitwatch.sh

--- lab6/src/main/java/ActorSys/ActorConf.java
+++ lab6/src/main/java/ActorSys/ActorConf.java
@@ -4,6 +4,10 @@ import akka.actor.AbstractActor;
 
 import java.util.ArrayList;
 
+final class MessageGetRandomServerUrl{
+    MessageGetRandomServerUrl(){}
+}
+
 public class ActorConf extends AbstractActor {
     private ArrayList<String> servers = new ArrayList<>();
 
@@ -11,7 +15,7 @@ public class ActorConf extends AbstractActor {
     public Receive createReceive() {
         return receiveBuilder()
                 .match(
-                        
+
                 )
     }
 }
