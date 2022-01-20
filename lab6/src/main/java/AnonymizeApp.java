30ed1f5674902369c118d6ca2470ac73728a7c9a
MaksimKoster <m.kostritsya@yandex.ru>
2022-01-20 03:07:07 +0300

    Scripted auto-commit on change (2022-01-20 03:07:07) by gitwatch.sh

--- lab6/src/main/java/AnonymizeApp.java
+++ lab6/src/main/java/AnonymizeApp.java
@@ -69,8 +69,6 @@ public class AnonymizeApp {
             System.exit(-1);
         }
 
-        System.out.println("Press Enter to stop\n"+ serversInfo);
-
         for(CompletionStage<ServerBinding> binding : bindings){
             binding.thenAccept(ServerBinding::unbind)
                     .thenAccept(unbound -> system.terminate());
